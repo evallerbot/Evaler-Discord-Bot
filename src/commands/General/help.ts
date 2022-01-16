b042d8db37788d6a03b6f6e795f9e9b009973724
Leo <38267386+randomlylelo@users.noreply.github.com>
2022-01-16 15:31:59 -0800

    help cmd -- start refactor

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -2,29 +2,30 @@ import { Args, ok, SapphirePrefix } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
 import { Message, MessageEmbed } from 'discord.js';
+import { inlineCodeBlock as iCB } from '@sapphire/utilities';
+import { blockQuote } from '@discordjs/builders';
 
-const defaultEmbed = (embed: MessageEmbed, prefix?: SapphirePrefix) => {
+const defaultEmbed = (embed: MessageEmbed, prefix: SapphirePrefix | undefined) => {
+	const adminCommands = blockQuote(
+		`${iCB('ban')} Bans a user from using the bot.
+    ${iCB('unban')} Unbans a user from using the bot.
+  `
+	);
+	const UserCommands = blockQuote(
+		`${iCB('eval')} Give it code and it will send back the result!
+    ${iCB('share')} Share your previously saved code!
+    ${iCB('list')} Lists all the previously saved evals!
+    ${iCB('del')} Deletes a previously saved eval!
+    ${iCB('save')} Allows you to save code without excution.
+    ${iCB('eval_saved')} or ${iCB('es')} for short, executes a previously saved code and re-executes the code and saves it.
+    `
+	);
+
 	embed
 		.setDescription('Type  `' + prefix + 'help <command-name>` to view command specific help.')
 		.addField('Prefix', '> The prefix for the bot is: `' + prefix + '`')
-		.addField(
-			'Admin Commands',
-			`
->>> \`ban\` Bans a user from using the bot.
-\`unban\` Unbans a user from using the bot.
-    `
-		)
-		.addField(
-			'User Commands',
-			`
->>> \`eval\` Give it code and it will send back the result!
-\`share\` Share your previously saved code!
-\`list\` Lists all the previously saved evals!
-\`del\` Deletes a previously saved eval!
-\`save\` Allows you to save code without excution.
-\`eval_saved\` or \`es\` for short, executes a previously saved code and re-executes the code and saves it.
-    `
-		)
+		.addField('Admin Commands', adminCommands)
+		.addField('User Commands', UserCommands)
 		.setFooter({ text: 'Run on Repl.it', iconURL: 'https://i.imgur.com/89E6Sie.png' })
 		.setTimestamp();
 };
@@ -47,24 +48,22 @@ export class UserCommand extends Command {
 			.setFooter({ text: 'Run on Repl.it', iconURL: 'https://i.imgur.com/89E6Sie.png' })
 			.setTimestamp();
 
-    // If there are arguments then just do default
+		// If there are arguments then just do default
 		if (!specificCommand.success) {
 			defaultEmbed(helpPrompt, prefix);
 
 			return await message.reply({ embeds: [helpPrompt] });
 		}
 
-    // Get the first argument
+		// Get the first argument
 		switch (specificCommand.value[0]) {
 			/*
 			 * EVAL *
 			 */
 			case 'eval':
-				helpPrompt
-					.setDescription('Command `eval`!')
-					.addField(
-						'Syntax',
-						`
+				helpPrompt.setDescription('Command `eval`!').addField(
+					'Syntax',
+					`
 > eval <optional-name> 
 > \\\`\\\`\\\`[language-name]
 > [code]
@@ -88,7 +87,7 @@ Currently supported languages are,
  •Obrya
  •CookeyLang
 If the language name is not specified, an error is thrown.`
-					)
+				);
 
 				break;
 
@@ -96,17 +95,15 @@ If the language name is not specified, an error is thrown.`
 			 * SHARE *
 			 */
 			case 'share':
-				helpPrompt
-					.setDescription('Command `share`!')
-					.addField(
-						'Syntax',
-						`
+				helpPrompt.setDescription('Command `share`!').addField(
+					'Syntax',
+					`
 > \`${prefix}share [name]\`
 Fetches the code and output of eval named <name> if it was saved by you.
 If [name] does not exist, an error is thrown. You can use the \`${prefix}list\` command to see your saved evals.
 *Note that in order for share to run, you would need to have previously run \`${prefix}eval <name>\` to save your code that you want to share*
 					`
-					)
+				);
 
 				break;
 
@@ -114,15 +111,13 @@ If [name] does not exist, an error is thrown. You can use the \`${prefix}list\` 
 			 * LIST *
 			 */
 			case 'list':
-				helpPrompt
-					.setDescription('Command `list`!')
-					.addField(
-						'Syntax',
-						`
+				helpPrompt.setDescription('Command `list`!').addField(
+					'Syntax',
+					`
 > \`${prefix}list\`
 See your shared evals. That's it.
 					`
-					)
+				);
 
 				break;
 
@@ -130,15 +125,13 @@ See your shared evals. That's it.
 			 * DEL *
 			 */
 			case 'del':
-				helpPrompt
-					.setDescription('Command `del`!')
-					.addField(
-						'Syntax',
-						`
+				helpPrompt.setDescription('Command `del`!').addField(
+					'Syntax',
+					`
 > \`${prefix}del [name]\`
 Deletes the eval named [name].
 					`
-					)
+				);
 
 				break;
 
@@ -146,15 +139,13 @@ Deletes the eval named [name].
 			 * SAVE *
 			 */
 			case 'save':
-				helpPrompt
-					.setDescription('Command `save`!')
-					.addField(
-						'Syntax',
-						`
+				helpPrompt.setDescription('Command `save`!').addField(
+					'Syntax',
+					`
 > \`${prefix}save [name] [\`\`\`[code-language] [code] \`\`\`] \`
 Just saves the code that you inputed.
 					`
-					)
+				);
 
 				break;
 
@@ -163,16 +154,14 @@ Just saves the code that you inputed.
 			 */
 			case 'eval_saved':
 			case 'es':
-				helpPrompt
-					.setDescription('Command `eval_saved`!')
-					.addField(
-						'Syntax',
-						`
+				helpPrompt.setDescription('Command `eval_saved`!').addField(
+					'Syntax',
+					`
 > \`${prefix}eval_saved [name] \`
 > \`${prefix}es [name]\`
 Reruns the saved code, and overrides the previous saved result.
 					`
-					)
+				);
 
 				break;
 
@@ -180,16 +169,14 @@ Reruns the saved code, and overrides the previous saved result.
 			 * BAN *
 			 */
 			case 'ban':
-				helpPrompt
-					.setDescription('Command `ban`!')
-					.addField(
-						'Syntax',
-						`
+				helpPrompt.setDescription('Command `ban`!').addField(
+					'Syntax',
+					`
 > \`${prefix}ban [name] \`
 **ADMIN ONLY COMMAND**
 Bans a user from using the bot.
 					`
-					)
+				);
 
 				break;
 
@@ -208,7 +195,7 @@ Bans a user from using the bot.
 **ADMIN ONLY COMMAND**
 Unbans a user from using the bot.
 					`
-					)
+					);
 
 				break;
 
